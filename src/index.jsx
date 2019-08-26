24517638dffdfbb3ba35cd7a396cc3c243cb09b0
Overflow Cat <overflowcat@gmail.com>
2019-08-26 22:36:54 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
-import {Button, TextView, contentView} from 'tabris';
-
+import {Button, TextView, TextInput, contentView} from 'tabris';
+import * as core from './ManchuCore';
 contentView.append(
   <$>
     <Button center onSelect={showText}>Tap here</Button>
     <TextView centerX bottom='prev() 20' font='24px'/>
   </$>
 );
+
+new TextInput({
+  left: 16, right: 16,
+  keyboard: 'multiline',
+  message: 'Manchu or transcription'
+}).onInput(({text}) => console.log(`Text changed to ${text}`))
+  .appendTo(contentView);
+
 
 function showText() {
   $(TextView).only().text = 'Tabris.js rocks!';

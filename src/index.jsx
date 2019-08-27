e998023f273e9303b3daf19e5f95f240e862a674
Overflow Cat <overflowcat@gmail.com>
2019-08-27 22:44:12 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -241,7 +241,7 @@ function Manchurize(str) {
   }
   return tmp;
 }
-const hana ={image="./IMG_20190827_222335.webp"}
+//const hana ={image="./IMG_20190827_222335.webp"}
 import {Button, ImageView, TextView, TextInput, contentView} from 'tabris';
 //var core = require('./ManchuCore');
 contentView.append(
